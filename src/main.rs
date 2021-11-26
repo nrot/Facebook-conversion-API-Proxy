0be21212ffa2039463c98b102fc5f882732616db
nrot <nrot13@gmail.com>
2021-11-26 23:32:20 +0300

    Перешол на rocket и он работает с sqlx и на tokio v1.*

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use dotenv::dotenv;
 use env_logger;
@@ -7,28 +8,24 @@ use log::LevelFilter;
 use sqlx::sqlite::SqlitePoolOptions;
 use std::{env, thread};
 
-use sqlx::{sqlite, Connection, SqlitePool};
+use rocket::State;
+use sqlx::{sqlite, Connection, SqlitePool, Pool, Sqlite};
 
 mod api;
 mod auth;
 
 #[get("/")]
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("")
+async fn index(pool: &State<Pool<Sqlite>>) -> String {
+    String::from("Hello world!")
 }
 
-#[launch]
-async fn main() -> std::io::Result<()> {
+#[rocket::main]
+async fn main() {
     dbgs!("Debug server");
 
     println!("Server start");
 
     dotenv().ok();
-
-    let cpus = env::var("CPU_WORKERS")
-        .expect("CPU_WORKERS must be set in env")
-        .parse::<usize>()
-        .expect("CPU_WORKERS must be usize");
 
     let log_level = match env::var("LOG_LEVEL") {
         Ok(lvl) => match lvl.to_uppercase().trim() {
@@ -69,27 +66,15 @@ async fn main() -> std::io::Result<()> {
         "Try to connect sqlite DB by url: {}",
         db_url.as_str().clone()
     );
-    let sqlite_connection = match block_on(
-        SqlitePoolOptions::new()
-            .min_connections(sqlite_min_connection)
-            .max_connections(sqlite_max_connection)
-            .connect(db_url.as_str())
-    ) {
+    let sqlite_connection = match SqlitePoolOptions::new()
+        .min_connections(sqlite_min_connection)
+        .max_connections(sqlite_max_connection)
+        .connect(db_url.as_str())
+        .await
+    {
         Ok(c) => c,
         Err(e) => panic!("Error by connect db: {}", e),
     };
 
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .data(sqlite_connection.clone())
-            .wrap(auth::TokenAuthInit::new(sqlite_connection.clone()))
-        // .service(hello)
-        // .service(echo)
-        // .route("/hey", web::get().to(manual_hello))
-    })
-    .workers(if cfg!(debug_assertions) { 1 } else { cpus })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    rocket::build().manage(sqlite_connection).mount("/", routes![index]).launch().await;
 }
